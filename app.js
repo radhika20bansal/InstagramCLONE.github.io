f084f8d3d6dda4f8c270f792f701f527bc859181
Pooja Rathore <poojarathore.nks@gmail.com>
2020-06-26 00:32:36 +0530

    Setting Up Mongoose DB

--- app.js
+++ app.js
@@ -15,20 +15,20 @@ mongoose.connection.on('error',(err)=>{
     console.log("error connecting to database!!",err);
 })
 //models i.e schema for mongodb
-require('./models/user');
-require('./models/post');
-//middlewares
-app.use(express.json())
-app.use(require('./routes/auth'))
-app.use(require('./routes/post'))
-app.use(require('./routes/user'))
-if(process.env.NODE_ENV=="production"){
-    app.use(express.static('client/build'))
-    const path=require('path')
-    app.get("*",(req,res)=>{
-        res.sendFile(path.resolve(__dirname,'client','build','index.html'))
-    })
-}
+// require('./models/user');
+// require('./models/post');
+// //middlewares
+// app.use(express.json())
+// app.use(require('./routes/auth'))
+// app.use(require('./routes/post'))
+// app.use(require('./routes/user'))
+// if(process.env.NODE_ENV=="production"){
+//     app.use(express.static('client/build'))
+//     const path=require('path')
+//     app.get("*",(req,res)=>{
+//         res.sendFile(path.resolve(__dirname,'client','build','index.html'))
+//     })
+// }
 
 
 app.listen(port_no,(req,res)=>{
